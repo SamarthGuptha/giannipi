b4bc106a1140cb699a56cb9ae1f5d1c0e5ad0136
Samarth Guptha <developervirusly@gmail.com>
2025-10-27 22:15:54 +0530

    hosting

--- src/wsgi.py
+++ src/wsgi.py
@@ -2,4 +2,4 @@ import os
 from app import app
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 4000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port) #thispushdontcount
